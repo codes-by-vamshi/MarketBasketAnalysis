d3c82eaf3a638ae0d0d22f59a19dd99da8fd856a
codes-by-vamshi <krishnavamshidevarasetty@gmail.com>
2023-10-28 23:24:40 +0530

    Setup of FE done

---
+++ src/App.js
@@ -0,0 +1,44 @@
+const items = {
+  1: 'Alfajores',
+  2: 'Baguette',
+  3: 'Bread',
+  4: 'Brownie',
+  5: 'Cake',
+  6: 'Chicken Stew',
+  7: 'Coffee',
+  8: 'Coke',
+  9: 'Cookies',
+  10: 'Focaccia',
+  11: 'Frittata',
+  12: 'Fudge',
+  13: 'Hot Chocolate',
+  14: 'Jam',
+  15: 'Jammie Dodgers',
+  16: 'Juice',
+  17: 'Lingonberries',
+  18: 'Medialuna',
+  19: 'Mineral Water',
+  20: 'Muffin',
+  21: 'Pastry',
+  22: 'Salad',
+  23: 'Sandwich',
+  24: 'Scone',
+  25: 'Smoothies',
+  26: 'Soup',
+  27: 'Spanish Brunch',
+  28: 'Tea',
+  29: 'Toast',
+  30: 'Truffles',
+  31: 'Vegan mincepie'
+}
+const App = () => {
+  return (
+    <div>
+      <div>
+        <h1>Market Basket Analysis</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
